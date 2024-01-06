e28dc811719d6155a5f0af3b5fd3597fb41ffdd8
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-06 13:38:42 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springboot/webclient/SpringBootWebclientApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebclientApplication.class, args);
+    }
+
+}
